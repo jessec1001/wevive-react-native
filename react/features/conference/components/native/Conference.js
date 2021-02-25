f6dc90e589d5aad0643796bb9b3a78a476e5d20b
jessec1001 <jessecorralejr@gmail.com>
2021-02-25 16:21:28 +0300

    Revert "removed unused modules"
    This reverts commit 63bfb2cf6e1448a20f41cf5e9df70b922ae21bf7.

--- react/features/conference/components/native/Conference.js
+++ react/features/conference/components/native/Conference.js
@@ -10,7 +10,7 @@ import { Container, LoadingIndicator, TintedView } from '../../../base/react';
 import { connect } from '../../../base/redux';
 import { ASPECT_RATIO_NARROW } from '../../../base/responsive-ui/constants';
 import { TestConnectionInfo } from '../../../base/testing';
-//import { ConferenceNotification, isCalendarEnabled } from '../../../calendar-sync';
+import { ConferenceNotification, isCalendarEnabled } from '../../../calendar-sync';
 import { Chat } from '../../../chat';
 import { DisplayNameLabel } from '../../../display-name';
 import { SharedDocument } from '../../../etherpad';
@@ -217,7 +217,10 @@ class Conference extends AbstractConference<Props, *> {
     _renderConferenceNotification() {
         const { _calendarEnabled, _reducedUI } = this.props;
 
-        return undefined;
+        return (
+            _calendarEnabled && !_reducedUI
+                ? <ConferenceNotification />
+                : undefined);
     }
 
     /**
